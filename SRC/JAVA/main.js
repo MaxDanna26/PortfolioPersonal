6fe577968639f2cb9851b097c205e9192b4d6e58
MaxDanna26 <maximilianodanna@hotmail.com>
2024-05-25 22:03:39 +0200

    Adding some animations

--- SRC/JAVA/main.js
+++ SRC/JAVA/main.js
@@ -3,49 +3,49 @@ const abrir = document.querySelector("#abrir")
 const cerrar = document.querySelector("#cerrar")
 const boton = document.querySelector("#btn")
 const nosotros = document.querySelector("#nosotros-imagen")
-
-const fullStack = document.getElementById("h3")
-
+const btnAbout = document.getElementById("btnAbout")
+const aboutmeP = document.getElementById("aboutmeP")
+const btnOculto = document.getElementById("btnOculto")
 let date = new Date()
 
-abrir.addEventListener('click', () =>{
-    nav.classList.add("visible")
-})
-
-cerrar.addEventListener('click', () =>{
-    nav.classList.remove("visible")
-})
-
-abrir.addEventListener('click', () =>{
-    btn.classList.add('invisible')
-})
-
-cerrar.addEventListener('click', () =>{
-    btn.classList.remove('invisible')
-})
-
-abrir.addEventListener('click', () =>{
-    nosotros.classList.add('invisible')
-})
-
-cerrar.addEventListener('click', () =>{
-    nosotros.classList.remove('invisible')
-})
-
-const content = document.getElementById('h3');
-let lastScrollTop = 0;
-
-document.addEventListener('scroll', () => {
-  const scrollTop = window.scrollY || document.documentElement.scrollTop;
-
-  if (scrollTop > lastScrollTop) {
-    content.classList.add('animate__rubberBand');
-  } else {
-    content.classList.remove('animate__rubberBand');
-  }
-
-  lastScrollTop = scrollTop;
+// Función para alternar una clase en un elemento
+function toggleClass(element, className) {
+  element.classList.toggle(className);
+}
+btnAbout.textContent = "Leer mas ->"
+// Llamo a la funcion para mostrar/ocultar secciones y botón
+btnAbout.addEventListener('click', () => {
+  if(btnAbout.textContent == "Leer menos <-")
+    btnAbout.textContent = "Leer mas ->"
+  else if(btnAbout.textContent == "Leer mas ->")
+    btnAbout.textContent = "Leer menos <-";
+  toggleClass(aboutmeP, "oculto");
 });
 
-// const copyright = document.getElementById('copyright')
-// copyright.innerHTML = `All Rights Reserved ${date.getFullYear()} &copy;`;
+// Llamo a la funcion para mostrar/ocultar el menú de navegación
+abrir.addEventListener('click', () => {
+  toggleClass(nav, "visible");
+});
+
+cerrar.addEventListener('click', () => {
+  toggleClass(nav, "visible");
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const elements = document.querySelectorAll('.scroll-element');
+
+  const revealOnScroll = () => {
+      const scrollPosition = window.scrollY + window.innerHeight * 0.8;
+
+      elements.forEach(element => {
+          if (element.offsetTop < scrollPosition) {
+            element.classList.add('visible');
+          } else {
+              element.classList.remove('visible');
+          }
+      });
+  };
+
+  window.addEventListener('scroll', revealOnScroll);
+  revealOnScroll(); // Para verificar la visibilidad inicial
+});
